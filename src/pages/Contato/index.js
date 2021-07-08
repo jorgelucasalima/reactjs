e491f3e6d795005967a40aa5bd20a3413c465c17
jorgelucasalima <jorgelucasalima@gmail.com>
2021-07-08 16:22:27 -0300

    criado links para pages

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+
+
 export default function Contato() {
     return (
         <div>
             <h1>Bem vindo aos CONTATOS</h1> <br/>
-            <span>Email: teste@teste.com.br</span>
+            <span>Email: teste@teste.com.br</span><br/>
+            <Link to="/">Home</Link><br/>
+            <Link to="/sobre">Sobre</Link><br/>
 
         </div>
     )
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,7 +1,12 @@
+
+import { Link } from "react-router-dom"
 export default function Sobre() {
     return (
         <div>
             <h1>Bem vindo ao SOBRE</h1>
+            <br/>
+            <Link to="/">Home</Link><br/>
+            <Link to="/contato">Contato</Link>
         </div>
     )
 }
